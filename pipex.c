335ab6343a8506277e37444298820fb2e065c566
mouadsrk <mouad.srk8@gmail.com>
2023-02-08 21:00:33 +0100

    prototype 1

--- pipex.c
+++ pipex.c
@@ -1,6 +1,6 @@
- #include "pipex.h"
+#include "pipex.h"
 
-char **ft_path(char *envp[])
+char **ft_all_paths(char *envp[])
 {
 	int i;
 
@@ -13,62 +13,108 @@ char **ft_path(char *envp[])
 	return NULL;
 }
 
-// void ft_commande1(t_vars *v)
-// {
-// 	int fd ;
-// 	char **cmd;
-// 	char *path;
+void ft_path(t_vars *v,char *argv)
+{
+	int i;
 
-// 	fd = open(v->arg[1],O_RDONLY,0777);
-// 	cmd = ft_split(v->arg[2],' ');
-// 	char *new_argv[] = {cmd, NULL };
-//     char *new_envp[] = { "PATH=/bin", "TERM=xterm", NULL };
-// 	path = ft_strjoin("/bin/",cmd[0]);
-//     execve(path, new_argv, new_envp);			
-// }
+	i = 0;
+	v->cmd1 = ft_split(argv,' ');
+	while (v->paths[i])
+	{
+		v->cmdpath = ft_strjoin(v->paths[i], v->cmd1[0]);
+		if(!access(v->cmdpath,R_OK))
+		{
+			break;
+		}
+		free(v->cmdpath);
+		i++;
+	}
+}
 
-// void ft_commande2(t_vars *v)
-// {
-// 	char *new_argv[] = { "pwd", NULL };
-//     char *new_envp[] = { "PATH=/bin", "TERM=xterm", NULL };
+void ft_commande1(t_vars *v,char **argv)
+{
+	int	fdinput;
 
-//     execve("/bin/pwd", new_argv, new_envp);
-// }
-// void ft_pipex(t_vars *v)
-// {	
-// 	pipe(v->fd);
-// 	v->p1pid = fork();
-// 	if(v->p1pid == 0)
-// 	{
-// 		close(v->fd[0]);
-// 		dup2(v->fd[1],STDOUT_FILENO);
-// 		ft_commande1(v);
-// 	}
-// 	else
-// 		wait(NULL);
-// 		ft_command2();
-// }
+	fdinput = open(argv[1],O_RDONLY,0777);
+	if (fdinput == -1) {
+        perror("Error opening input file");
+        exit(1);
+    }
+	dup2(fdinput,STDIN_FILENO);
+	dup2(v->fd[1],STDOUT_FILENO);
+	close(v->fd[1]);
+	close(fdinput);
+    execve(v->cmdpath, v->cmd1, NULL);
+}
+
+
+void ft_commande2(t_vars *v,char **argv)
+{
+	int	fd;
+
+	fd = open(argv[4],O_WRONLY | O_TRUNC,0777);
+	if (fd == -1) {
+        perror("Error opening input file");
+        exit(1);
+    }
+	// int	fdinput;
+
+	// fdinput = open(argv[1],O_RDONLY ,0777);
+	// if (fdinput == -1) {
+    //     perror("Error opening input file");
+    //     exit(1);
+    // }
+	// 	char buffer[10];
+    //     int n;
+	// 	n = 1;
+	// 	int i = 0;
+    // while ((n = read(v->fd[0], buffer, 10)) > 0 && i++ <30)
+	// {
+    //     // write(1, buffer, n);
+	// }
+	dup2(v->fd[0],STDIN_FILENO);
+	dup2(fd,STDOUT_FILENO);
+	close(v->fd[1]);
+	close(fd);
+	close(v->fd[0]);
+    execve(v->cmdpath, v->cmd1, NULL);
+}
+
+void ft_pipex(t_vars *v, char **argv)
+{
+	int status;
+
+	if (pipe(v->fd) == -1) {
+        perror("Error creating pipe");
+        exit(1);
+	}
+	v->p1pid = fork();
+    if (v->p1pid == -1) {
+        perror("Error creating child process");
+        exit(1);
+    }
+	if(v->p1pid == 0)
+	{
+		close(v->fd[0]);
+		ft_path(v,argv[2]);
+		ft_commande1(v,argv);
+	}
+	else
+	{
+		wait(0);
+		ft_path(v,argv[3]);
+		ft_commande2(v,argv);
+		exit(0);
+	}	
+}
 
 int main (int argc ,char **argv,char *envp[])
 {
 	t_vars v;
-
-    // if(argc < 5)
-	// 	return 0;
-	v.path=ft_path(envp);
-	// ft_pipex(v);
 	int i = 0;
-	while (v.path[i])
-	{
-
-		v.cmd = ft_strjoin(v.path[i], argv[2]);
-		if(!access(v.cmd,R_OK))
-		{
-			printf("<%s>",v.cmd);
-			break;
-		}
-		free(v.cmd);
-		i++;
-	}
+    if(argc < 5)
+		return 0;
+	v.paths=ft_all_paths(envp);
+	ft_pipex(&v,argv);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -1,19 +1,24 @@
 #ifndef PIPEX_H
 #define	PIPEX_H
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#define BUFFER_SIZE 42
 
 typedef struct var_s
 {
-	char	**path;
-	char	*cmd;
+	char	**paths;
+	char	*cmdpath;
+	char	**cmd1;
 	int		fd[2];
 	int		p1fd;
 	int		p1pid;
 	int		p2pi;
+	int		p2fd;
 
 }t_vars;
 
@@ -23,4 +28,3 @@ char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 #endif
-
